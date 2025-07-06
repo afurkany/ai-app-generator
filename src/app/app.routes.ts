fdb52712a59a00b829a20e6ae96976d15b60bcd5
afurkany <yildiz.furkan.a@gmail.com>
2025-07-06 17:29:51 +0300

    feat(): side nav implementation done.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ProjectPageComponent } from "./project-page/project-page.component";
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+	{ path: '', component: HomePageComponent },
+	{ path: 'project', component: ProjectPageComponent }
+];
